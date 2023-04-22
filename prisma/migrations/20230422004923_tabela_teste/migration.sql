f74f0c4c19a6f5f7de260d9a67994869a32e25bf
RudhyDev <rudhy.pereira@gmail.com>
2023-04-21 21:57:24 -0300

    migration e scripts de ambientes para o prisma

---
+++ prisma/migrations/20230422004923_tabela_teste/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "Tabela_teste" (
+    "id" TEXT NOT NULL,
+    "nome" TEXT NOT NULL,
+    "sobrenome" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "senha" TEXT NOT NULL,
+
+    CONSTRAINT "Tabela_teste_pkey" PRIMARY KEY ("id")
+);
